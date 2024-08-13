920b6424ef94cd0d286788a4f17b01c9738e0c10
Alex <alessio.maiola@yahoo.it>
2024-08-13 03:45:16 +0200

    pentaho part done

--- tables.sql
+++ tables.sql
@@ -33,6 +33,38 @@ CREATE TABLE songartist (
     PRIMARY KEY (id_song, id_artist)
 );
 
+-- Creazione della tabella songSpotify
+CREATE TABLE songSpotify (
+    id_song INTEGER NOT NULL,
+    uri VARCHAR(255),
+    bpm NUMERIC,
+    danceability NUMERIC,
+    loudness NUMERIC,
+    liveness NUMERIC,
+    acousticness NUMERIC,
+    speechiness NUMERIC,
+    instrumentalness NUMERIC,
+    key INTEGER,
+    duration BIGINT,
+    popularity INTEGER,
+    explicit BOOLEAN,
+    added DATE,
+    top_year INTEGER,
+    streams BIGINT
+);
+
+CREATE TABLE songYouTube (
+    id_song INTEGER NOT NULL,
+    url VARCHAR(255),
+    channel VARCHAR(255),
+    views BIGINT,
+    likes BIGINT,
+    comments BIGINT,
+    description TEXT,
+    licensed BOOLEAN,
+    official_video BOOLEAN
+);
+
 
 -- Aggiungi il vincolo di chiave esterna per id_album
 ALTER TABLE albumartist
@@ -57,3 +89,12 @@ FOREIGN KEY (id_song) REFERENCES song(id_song);
 ALTER TABLE songartist
 ADD CONSTRAINT fk_artist
 FOREIGN KEY (id_artist) REFERENCES artist(id_artist);
+
+-- Aggiunta del vincolo di chiave esterna
+ALTER TABLE songSpotify
+ADD CONSTRAINT fk_song
+FOREIGN KEY (id_song) REFERENCES song(id_song);
+
+ALTER TABLE songYouTube
+ADD CONSTRAINT fk_song
+FOREIGN KEY (id_song) REFERENCES song(id_song);
